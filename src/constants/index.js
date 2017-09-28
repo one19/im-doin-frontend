99e6a8f50eed95a02e0c5eeab1fd870a7b87bf8d
Drew Showalter <maelstroswim@gmail.com>
2017-09-29 00:45:48 +1000

    :recycle: rework big constant objects into own space.

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export * from './progressive_punctuations';
+export * from './company_colors';
---
+++ src/constants/progressive_punctuations.js
@@ -0,0 +1,58 @@
+module.exports.PROGRESSIVE_PUNCTS = {
+  '&#57345;': {
+    alias: [':acclamation:', '/!!', '/gusto'],
+    url: 'http://progressivepunctuation.com/mark/#acclamation'
+  },
+  '&#57344;': {
+    alias: [':authority:', '/:!', '/!:', '/sudo'],
+    url: 'http://progressivepunctuation.com/mark/#authority'
+  },
+  '&#57347;': {
+    alias: [':certitude:', '/..', '/+.', '/.+', '/fosho'],
+    url: 'http://progressivepunctuation.com/mark/#certitude'
+  },
+  '&#57350;': {
+    alias: [':comma-bang:', '/,!', '/!,', '/!;', '/;!'],
+    url: 'http://progressivepunctuation.com/mark/#exclamation-comma'
+  },
+  '&#57348;': {
+    alias: [':doubt:', '/.?', '/?.'],
+    url: 'http://progressivepunctuation.com/mark/#doubt'
+  },
+  '&#57351;': {
+    alias: [':dot-friend:', '/(:', '/.(:'],
+    url: 'http://progressivepunctuation.com/mark/#friendly-period'
+  },
+  '&#57354;': {
+    alias: [':dot-love:', '/<3', '/.<3'],
+    url: 'http://progressivepunctuation.com/mark/#love-point'
+  },
+  '&#57349;': {
+    alias: [':elrey:', '/.!', '/!.', '/ok'],
+    url: 'http://progressivepunctuation.com/mark/#elrey'
+  },
+  '&#57352;': {
+    alias: [':interrobang:', '/?!', '/!?'],
+    url: 'http://progressivepunctuation.com/mark/#interrobang'
+  },
+  '&#57353;': {
+    alias: [':irony:', '/i'],
+    url: 'http://progressivepunctuation.com/mark/#irony'
+  },
+  '&#57355;': {
+    alias: [':interrocomma:', '/?,', '/,?'],
+    url: 'http://progressivepunctuation.com/mark/#question-comma'
+  },
+  '&#57356;': {
+    alias: [':interrorhetoric:', ':rhetorical:', '/r'],
+    url: 'http://progressivepunctuation.com/mark/#rhetorical-question'
+  },
+  '&#57357;': {
+    alias: [':sarcmark:', ':sarcasm:', '/s'],
+    url: 'http://progressivepunctuation.com/mark/#sarcmark'
+  },
+  '&#57346;': {
+    alias: [':snarkmark:', '/lol', '/jk'],
+    url: 'http://progressivepunctuation.com/mark/#snark-mark'
+  }
+};
