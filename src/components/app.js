1035710c9b1d5d1a7fdb867fd8afaaeab4e6b1b6
Drew Showalter <maelstroswim@gmail.com>
2017-09-29 00:39:45 +1000

    :recycle: remove firebase cdn import for bundling.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import { h, Component } from 'preact';
 import styled from 'styled-components';
+import firebase from 'firebase';
+import tc from 'tinycolor2';
 import snarkdown from 'snarkdown';
 import Background from './background';
 import H1 from './h1';
-import { backgroundCSSGenerator } from '../utils';
+import { backgroundCSSGenerator, customTextParser } from '../utils';
 
 const config = {
   apiKey: 'AIzaSyCfSoWWocJ9JY9empbh8uxsJGN6Kc9pWIk',
@@ -22,6 +24,25 @@ const AbsoluteCanvas = styled.canvas`
   bottom: 0;
   overflow: auto;
 `;
+const AppReadable = styled.div`
+  -webkit-text-stroke: unset;
+  -webkit-text-fill-color: unset;
+  ${p => {
+    const isColor =
+      typeof p.background === 'string' &&
+      p.background.match(/background-color: (.*)/);
+
+    // return the most readable text color for the background
+    if (isColor) {
+      const color = tc.mostReadable(isColor[1], ['#fff', '#000']);
+      return `color: ${color}`;
+    }
+
+    // default to white text for images/canvasses
+    return `-webkit-text-stroke: 2px #000;
+            -webkit-text-fill-color: #fff`;
+  }};
+`;
 
 export default class App extends Component {
   componentWillMount() {
@@ -34,17 +55,19 @@ export default class App extends Component {
 
   render() {
     const { message = 'Loading...', background } = this.state;
-    const mdMessage = snarkdown(message.replace(/(in)g(?![A-Za-z])/gi, "$1'"));
+    const mdMessage = snarkdown(customTextParser(message));
     const parsedBackground = backgroundCSSGenerator(background);
 
     return (
-      <div>
+      <AppReadable background={parsedBackground}>
         <AbsoluteCanvas id="trianglify" />
         <Background background={parsedBackground} />
-        <H1 background={parsedBackground}>
-          <div dangerouslySetInnerHTML={{ __html: mdMessage }} />
+        <H1>
+          <div
+            dangerouslySetInnerHTML={{ __html: customTextParser(mdMessage) }}
+          />
         </H1>
-      </div>
+      </AppReadable>
     );
   }
 }
