3e96bdcdf0f1c65e8a20dc3618d01cec5d4692fb
Drew Showalter <maelstroswim@gmail.com>
2017-09-29 00:45:02 +1000

    :nail_care: rework imports and style flow to h1.

--- src/components/h1.js
+++ src/components/h1.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import tc from 'tinycolor2';
 
 export default styled.h1`
   font-family: 'helvetica';
@@ -8,20 +7,7 @@ export default styled.h1`
   position: absolute;
   text-align: center;
   top: 50vh;
-  color: #000;
-  ${p => {
-    const isColor =
-      typeof p.background === 'string' &&
-      p.background.match(/background-color: (.*)/);
-
-    // return the most readable text color for the background
-    if (isColor) {
-      const color = tc.mostReadable(isColor[1], ['#fff', '#000']);
-      return `color: ${color}`;
-    }
-
-    // default to white text for images/canvasses
-    return `-webkit-text-stroke: 2px #000;
-            -webkit-text-fill-color: #fff`;
-  }};
+  color: inherit;
+  -webkit-text-stroke: inherit;
+  -webkit-text-fill-color: inherit;
 `;
--- src/utils.js
+++ src/utils.js
@@ -1,71 +1,22 @@
 /* @flow */
-const tc = require('tinycolor2');
-const validUrl = require('valid-url');
-const trianglify = require('trianglify');
+import tc from 'tinycolor2';
+import validUrl from 'valid-url';
+import trianglify from 'trianglify';
+import { COMPANY_COLORS, PROGRESSIVE_PUNCTS } from './constants';
 
-const CONSTANTS = {
-  airbnb: '#fd5c63',
-  amazon: ['#146eb4', '#ff9900'],
-  angular: ['#000000', '#b52e31'],
-  canon: ['#000000', '#bc0024'],
-  discord: ['#99aab5', '#7289da'],
-  docker: ['#384d54', '#0db7ed'],
-  facebook: '#3b5998',
-  gatech: ['#c59353', '#fff', '#eeb211', '#00254c'],
-  hackernews: '#ff6600',
-  hangouts: '#0f9d58',
-  heroku: ['#c9c3e6', '#6762a6'],
-  html: '#e34f26',
-  iftt: ['#e34f26', '#ff4400'],
-  ikea: ['#ffcc00', '#003399'],
-  instagram: [
-    '#405de6',
-    '#5851db',
-    '#833ab4',
-    '#c13584',
-    '#e1306c',
-    '#fd1d1d',
-    '#f56040',
-    '#f77737',
-    '#fcaf45',
-    '#ffdc80'
-  ],
-  jquery: ['#7acef4', '#0769ad'],
-  kickstarter: ['#0f2105', '#2bde73'],
-  linkedin: '#0077b5',
-  medium: '#00ab6c',
-  netflix: '#e50914',
-  node: '#6cc24a',
-  npm: '#cb3837',
-  paypal: '#003087',
-  raspi: ['#8cc04b', '#c7053d'],
-  razer: '#00ff00',
-  react: '#00d8ff',
-  reddit: ['#ff4500', '#5f99cf'],
-  shortlyster: '#203d57',
-  skype: '#00aff0',
-  slack: ['#3eb991', '#e01563', '#e9a820', '#6ecadc'],
-  stripe: '#00afe1',
-  snapchat: '#fffc00',
-  tumblr: '#35465c',
-  twitter: '#1da1f2',
-  vue: ['#42b883', '#35495e'],
-  wordpress: ['#464646', '#d54e21', '#21759b']
-};
-
-module.exports.backgroundCSSGenerator = (background: string): string => {
-  if (Object.keys(CONSTANTS).includes(background)) {
-    return Array.isArray(CONSTANTS[background])
+module.exports.backgroundCSSGenerator = (background: string) => {
+  if (Object.keys(COMPANY_COLORS).includes(background)) {
+    return Array.isArray(COMPANY_COLORS[background])
       ? trianglify({
           seed: 'one19',
           variance: 0.5,
           cell_size: 45,
           width: window.innerWidth,
           height: window.innerHeight,
-          x_colors: CONSTANTS[background],
-          y_colors: CONSTANTS[background]
+          x_colors: COMPANY_COLORS[background],
+          y_colors: COMPANY_COLORS[background]
         })
-      : `background-color: ${CONSTANTS[background]}`;
+      : `background-color: ${COMPANY_COLORS[background]}`;
   } else if (tc(background).isValid()) {
     return `background-color: ${tc(background).toString()}`;
   } else if (validUrl.isUri(background)) {
@@ -77,4 +28,29 @@ module.exports.backgroundCSSGenerator = (background: string): string => {
   return `background-color: ${tc('palegoldenrod')
     .spin(360 * Math.random())
     .toString()}`;
+};
+
+const insertProgressivePunct = (text: string): string =>
+  Object.keys(PROGRESSIVE_PUNCTS).reduce(
+    (returnText, key) =>
+      PROGRESSIVE_PUNCTS[key].alias.reduce((innerText, alia) => {
+        const safeKey = alia.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+
+        let keyRegex = new RegExp(safeKey, 'ig');
+        if (alia.match(/\/[a-z]$/))
+          keyRegex = new RegExp(`[^<]/${alia[1]}`, 'ig');
+
+        return innerText.replace(
+          keyRegex,
+          `<a href="${PROGRESSIVE_PUNCTS[key].url}"><icon>${key}</icon></a>`
+        );
+      }, returnText),
+    text
+  );
+
+module.exports.customTextParser = (text: string): string => {
+  const imDoinIfied = text.replace(/(in)g(?![A-Za-z])/gi, "$1'");
+  const withStrikethroughs = imDoinIfied.replace(/~~(.*)~~/, '<del>$1</del>');
+  const withProgPunct = insertProgressivePunct(withStrikethroughs);
+  return withProgPunct;
 };
